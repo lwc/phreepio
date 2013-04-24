b064c53679e517d2379a508dab0122c606196684
Luke Cawood <luke.cawood@99designs.com>
2013-04-25 00:36:56 +1000

    Add the ability to define custom placeholders

--- lib/Phreepio/Translator/SmartlingTranslator.php
+++ lib/Phreepio/Translator/SmartlingTranslator.php
@@ -8,11 +8,15 @@ class SmartlingTranslator implements Adapter
 {
     private $client;
     private $autoApprove;
+    private $placeholderFormat;
 
     public function __construct($config)
     {
+        $useSandbox = isset($config->useSandbox) ? $config->useSandbox : true;
+        $this->placeholderFormat = isset($config->placeholderFormat) ? $config->placeholderFormat : null;
+
         $this->autoApprove = isset($config->autoApprove) ? $config->autoApprove : false ;
-        $this->client = new Client($config->apiKey, $config->projectId, $config->useSandbox);
+        $this->client = new Client($config->apiKey, $config->projectId, $useSandbox);
     }
 
     public function download($remotePath, $localPath, $locale)
@@ -22,7 +26,11 @@ class SmartlingTranslator implements Adapter
 
     public function upload($localPath, $remotePath, $type)
     {
-        return (object)$this->client->upload($localPath, $remotePath, $type, $this->autoApprove);
+        $options = array();
+        if ($this->placeholderFormat) {
+            $options['placeholder_format_custom'] = $this->placeholderFormat;
+        }
+        return (object)$this->client->upload($localPath, $remotePath, $type, $this->autoApprove, $options);
     }
 
     public function status($remotePath, $locale)
